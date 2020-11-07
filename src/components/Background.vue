bf1807b4d2ae90088b849a6f7ca121c17fbd740d
Patryk Kaca <patrykaca@gmail.com>
2020-11-08 00:59:06 +0100

    Revert "background"
    This reverts commit 2b1ea5e6

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bgWrapper"/>
+
 </template>
 
 <script>
@@ -8,16 +8,6 @@ name: "Background"
 }
 </script>
 
-<style lang="scss" scoped>
-  .bgWrapper {
-    position: fixed;
-    top: 0;
-    z-index: -1;
-    width: 100%;
-    height: 100vh;
-    background-image: url("https://images.pexels.com/photos/305821/pexels-photo-305821.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: 24% 0;
-  }
+<style scoped>
+
 </style>
