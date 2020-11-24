a9dcabc5b19a2c3adf21f8095f40e520d469afa8
badwolf4 <badwolf43200@gmail.com>
2020-11-24 17:18:05 +0100

    dynamic list displaying

--- src/components/MoviesGenre.vue
+++ src/components/MoviesGenre.vue
@@ -1,101 +1,54 @@
 <template>
-    <div class="listWrapper">
-      <br/>
-      <h2>Films by genre</h2>
-      <!--
-        v-for="(index,val) in jsonData" :key="val"
 
-        {{jsonData[val]}}
-        -->
-      
-      <div class="accordion" id="accordion" 
-        v-for="(index,val) in jsonData" :key="val">
+    <div class="MoviesGenreList">
+    <!-- v-for="(index, val) in tableSize" :key="val" -->
+    <dl v-for="(index, val) in jsonData" :key="val" >
+      <dt @click="changeState(val)">{{jsonData[val]}}</dt>
+      <dd v-show="genreState[val]"> Here is some text</dd>
+    </dl>
 
-        <div class="accordion-item">
-
-          <h3 class="accordion-item-head">{{jsonData[val]}}</h3>
-          <div accordion-item-body>
-            {{jsonData[val]}}
-          </div>
-
-        </div>
-
-      </div>
     </div>
     
 </template>
 
 <script>
+//import _ from 'lodash';
+let genreState = []
+
 
 export default {
-  name: "MoviesGenreList",
   props: {
     jsonData: Array,
     jsonMovies: Array
   },
 
   data(){
+
+    for(let i=0; i<this.jsonData.length; i++){
+      genreState.push(false)
+    }
+    genreState[0]=true
+
+    genreState[3]=true
+    return {genreState}
     
-    return this.jsonData
   },
 
+
   methods: {
-  accordion: function (event) {            
- 
-        //Раскомментировать, если нужно, чтобы закрывались остальные пункты при открытии текущего
-         var matches = document.querySelectorAll('.accordion-item-head');
-         for(var i = 0; i < matches.length; i++) {
-             matches[i].classList.remove('active');
-          }
- 
-             event.target.classList.toggle('active');
-  }}
+    changeState: function(index){
+      genreState[index] = ! genreState[index]
+      console.log(genreState[index])
+      this.$forceUpdate();
+      //vm.$forceUpdate();
+    },
+
+  }
+  
 }
 
 </script>
 
 <style lang="scss" scoped>
 
-.listWrapper{
-  margin-top: 20px;
-  margin-left: 120px;
-  margin-right: 120px;
-  background-color: white;
-  opacity: 0.8;
-}
-
-.accordion-item{
-    position: relative;
-}
- 
-.accordion-item-head{
-    background-color: #dfdfdf;
-    border-top-left-radius: 5px ;
-    border-top-right-radius: 5px;
-    cursor: pointer;
-    padding:10px;
-}
- 
- 
-.accordion-item-head:after{    
-    content: ' > ';
-    display: block;
-    height: 29px;
-    position: absolute;
-    right: 25px;
-    transform: rotate(90deg) scaleY(2);
-    top: 8px;
-}
- 
-.accordion-item-head.active:after{
-    content: ' < ';
-}
- 
- 
-.accordion-item-body{
-    display: none;
-} 
-.accordion-item-head.active + .accordion-item-body{
-    display: block !important;
-}
 </style>
